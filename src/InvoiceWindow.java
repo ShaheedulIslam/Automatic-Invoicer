5879648b40cdbb4b233ffe734eaf8d1beb08d080
coder2468 <shaheedulislam20@gmail.com>
2015-09-26 17:48:23 +0100

    Able to add items now

--- src/InvoiceWindow.java
+++ src/InvoiceWindow.java
@@ -6,6 +6,7 @@ import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class InvoiceWindow extends JFrame{
 
@@ -38,8 +39,13 @@ public class InvoiceWindow extends JFrame{
     String[] columnNames;
     Object[][] data;
 
-    JButton jbClear;
     JButton jbSubmit;
+    JButton jbAddItem;
+
+    ArrayList<JTextField> alQTYTextField;
+    ArrayList<JTextField> alDescriptionTextField;
+    ArrayList<JTextField> alUnitPriceTextField;
+    ArrayList<JLabel> alLabels;
 
     public InvoiceWindow(){
         super("Invoice Program");
@@ -53,6 +59,11 @@ public class InvoiceWindow extends JFrame{
     }
 
     public void addComponents(){
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e){
+        }
 
         String[] columnNames = {"Item Description", "Quantity", "Price per Item", "Total"};
         Object[][] data = new Object[50][50];
@@ -81,11 +92,11 @@ public class InvoiceWindow extends JFrame{
         jlClientAddress.setBorder(new EmptyBorder(10, 10, 10, 10));
         jlClientName.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        jpMain = new JPanel(new GridLayout(6, 2));
-        jpButtons = new JPanel(new GridLayout(1, 2));
+        jpMain = new JPanel(new GridLayout(0, 2));
+        jpButtons = new JPanel(new GridLayout(1, 1));
 
-        jbClear = new JButton("Clear");
         jbSubmit = new JButton("Submit");
+        jbAddItem = new JButton("Add Item");
 
         jpMain.add(jlCompanyName);
         jpMain.add(jtCompanyName);
@@ -102,7 +113,7 @@ public class InvoiceWindow extends JFrame{
         jpMain.add(jlDate);
         jpMain.add(jtDate);
 
-        jpButtons.add(jbClear);
+        jpMain.add(jbAddItem);
         jpButtons.add(jbSubmit);
 
         add(jpMain, BorderLayout.NORTH);
@@ -110,18 +121,7 @@ public class InvoiceWindow extends JFrame{
     }
 
     public void addActions(){
-        jbClear.addActionListener(
-                new ActionListener() {
 
-                    public void actionPerformed(ActionEvent e) {
-                        jtCompanyName.setText("");
-                        jtCompanyAddress.setText("");
-
-                        jtClientName.setText("");
-                        jtClientAddress.setText("");
-                    }
-                }
-        );
 
         jbSubmit.addActionListener(
                 new ActionListener() {
@@ -130,11 +130,62 @@ public class InvoiceWindow extends JFrame{
                         sCompanyName = jtCompanyName.getText();
                         sCompanyAddress = jtCompanyAddress.getText();
 
-
                         sClientName = jtClientName.getText();
                         sClientAddress = jtClientAddress.getText();
+
+                        System.out.println("Company Name: " + sCompanyName);
+                        System.out.println("Company Address: " + sCompanyAddress);
+                        System.out.println("Client Name: " + sClientName);
+                        System.out.println("Client Name: " + sClientAddress);
+                        System.out.println();
                     }
                 });
+
+        jbAddItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jpMain.remove(jbAddItem);
+                revalidate();
+
+                alQTYTextField = new ArrayList<JTextField>();
+                alDescriptionTextField = new ArrayList<JTextField>();
+                alUnitPriceTextField = new ArrayList<JTextField>();
+                alLabels = new ArrayList<JLabel>();
+
+                JLabel jlQty = new JLabel("Quantity: ");
+                JLabel jlDescription = new JLabel("Description: ");
+                JLabel jlUnitPrice = new JLabel("Unit Price: ");
+
+                JTextField jtQty = new JTextField();
+                JTextField jtDescription = new JTextField();
+                JTextField jtUnitPrice = new JTextField();
+
+                alQTYTextField.add(jtQty);
+                alDescriptionTextField.add(jtDescription);
+                alUnitPriceTextField.add(jtUnitPrice);
+
+                alLabels.add(jlQty);
+                alLabels.add(jlDescription);
+                alLabels.add(jlUnitPrice);
+
+                jpMain.add(jlQty);
+                jpMain.add(jtQty);
+
+                jpMain.add(jlDescription);
+                jpMain.add(jtDescription);
+
+                jpMain.add(jlUnitPrice);
+                jpMain.add(jtUnitPrice);
+
+                jpMain.add(jbAddItem);
+
+                jlQty.setBorder(new EmptyBorder(10, 10, 10, 10));
+                jlDescription.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+                jlUnitPrice.setBorder(new EmptyBorder(10, 10, 10, 10));
+                revalidate();
+            }
+        });
     }
 
 
